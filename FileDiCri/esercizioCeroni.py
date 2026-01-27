f493fad667e26266a7c26068bb57493a10fde1ed
Cristina-Campo <cristina.campo@edu.immaginazioneelavoro.it>
2026-01-27 19:50:16 +0100

    finale

--- FileDiCri/esercizioCeroni.py
+++ FileDiCri/esercizioCeroni.py
@@ -3,34 +3,37 @@ print("BENVENUTO NEL GIORCO DI CARTA, SASSO FORBICE!")
 
 #Chiede il nome all'utente
 nomeUtente = input("Prima di tutto, inserisci il tuo nome = ")
+#Controlo che il nome abbia abbastanza caratteri e che sia valido
+if len(nomeUtente) >= 3:
+    print(f"{nomeUtente} preparati!")
 
+print("###########################################################################")
 #Chiede di inserire una mossa
-sceltaUtente = input("Ora scgli la tua mossa. Scrivi 'carta', 'sasso' o 'forbice = ")
+sceltaUtente = input("Ora scgli la tua mossa. Scrivi 'carta', 'sasso' o 'forbici = ")
 print(sceltaUtente)
 
 #Determinazione della mossa del PC
-sceltaPc = random.randint(0,2)
-mossaPc = ["carta" , "sasso" "forbici"]
-mossaPc = ""
-if sceltaPc == 1:
-    mossaPc = 
-    print(f"La mossa del pc è {mossaPc}")
-elif sceltaPc == 2:
-    mossaPc == "forbici"
-    print(f"La mossa del pc è {mossaPc}")
-elif sceltaPc == 3:
-    mossaPc == "sasso"
-    print(f"La mossa del pc è {mossaPc}")
-else:
-    print("Mossa non valida. ERRORE SUL RANDOM")     
+numRandom = random.randint(0,2)
+sceltaPc = ["carta" , "sasso", "forbici"]
 
+if numRandom == 0:
+    mossaPc = sceltaPc[0]
+    print(mossaPc)
+elif numRandom == 1:
+    mossaPc = sceltaPc[1]
+    print(mossaPc)
+elif numRandom == 2:
+    mossaPc = sceltaPc[2]
+    print(mossaPc)  
+else:
+    print("IDK MAAN")    
 
 #Determinazione del risultato
 
 if mossaPc == "carta" and sceltaUtente == "forbici" or mossaPc == "forbici" and sceltaUtente == "sasso" or mossaPc == "sasso" and sceltaUtente == "carta":
     print("Congratulazioni" , nomeUtente , "hai vinto!")
 elif mossaPc == "carta" and sceltaUtente == "sasso" or mossaPc == "forbici" and sceltaUtente == "carta" or mossaPc == "sasso" and sceltaUtente == "forbici":
-    print("Caspita" , nomeUtente , "! Andrà meglio la prossima volta!")
+    print("Mannaggia" , nomeUtente , "! Andrà meglio la prossima volta!")
 elif mossaPc == "carta" and sceltaUtente == "carta" or mossaPc == "forbici" and sceltaUtente == "forbici" or mossaPc == "sasso" and sceltaUtente == "sasso":
     print("Wow un pareggio!")        
 else:
