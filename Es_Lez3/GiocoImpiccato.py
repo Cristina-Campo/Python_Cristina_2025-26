65f345475b26f745deba1ae498ac5994d5216dbe
= <=>
2026-02-02 15:39:10 +0100

    Add hangman game implementation in GiocoImpiccato.py and update menu in 07_MENU.py

--- Es_Lez3/GiocoImpiccato.py
+++ Es_Lez3/GiocoImpiccato.py
@@ -1,31 +1,3 @@
-print("=== MENU, fai la tua scelta ===\n")
-
-scelta = ""
-
-while scelta != 4:
-    print("Menu")
-    print("1. Gioca con i numeri")
-    print("2. Gioca con i dadi")
-    print("3. Gioca con il pc")
-    print("4. ESCI")
-
-    scelta = int( input("Scegli: \n"))
-
-    if scelta == 1:
-        print("Stai giocando con i numeri...")
-    elif scelta == 2:
-        print("Stai lanciando i dadi...")
-    elif scelta == 3:
-        print("Stai giocando a solitario")
-    elif scelta == 4:
-        print("Ciao, alla prossima")
-    else:
-        print( "Non ho capito la scelta!! ")
-
-print("Programma terminato")
-
-# Testo di fine file
-
 parola = "appartamento"
 
 parolaARR = [] #Questo mi serve per trasformare la parola in un array
@@ -56,5 +28,3 @@ while(tentativi <= 6):
     if not parolaARR.__contains__(letteraCercata):
         tentativi += 1
         print(f"Ti restano {6 - tentativi} tentativi")
-
-
